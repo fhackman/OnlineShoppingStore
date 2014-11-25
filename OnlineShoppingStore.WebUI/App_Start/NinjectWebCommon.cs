552767e4892e45cc693ecc769d46f8a43363aa78
fhackman <natanons@gmail.com>
2014-11-25 11:33:11 +0700

    UpDateNuget

--- OnlineShoppingStore.WebUI/App_Start/NinjectWebCommon.cs
+++ OnlineShoppingStore.WebUI/App_Start/NinjectWebCommon.cs
@@ -3,78 +3,64 @@
 
 namespace OnlineShoppingStore.WebUI.App_Start
 {
-     using System;
-     using System.Web;
+    using System;
+    using System.Web;
 
-     using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
 
-     using Ninject;
-     using Ninject.Web.Common;
-     using OnlineShoppingStore.Domain.Abstract;
-     using Moq;
-     using OnlineShoppingStore.Domain.Entities;
-     using System.Collections.Generic;
-     using OnlineShoppingStore.Domain.Concrete;
+    using Ninject;
+    using Ninject.Web.Common;
 
-     public static class NinjectWebCommon
-     {
-          private static readonly Bootstrapper bootstrapper = new Bootstrapper();
+    public static class NinjectWebCommon 
+    {
+        private static readonly Bootstrapper bootstrapper = new Bootstrapper();
 
-          /// <summary>
-          /// Starts the application
-          /// </summary>
-          public static void Start()
-          {
-               DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
-               DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
-               bootstrapper.Initialize(CreateKernel);
-          }
+        /// <summary>
+        /// Starts the application
+        /// </summary>
+        public static void Start() 
+        {
+            DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
+            DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
+            bootstrapper.Initialize(CreateKernel);
+        }
+        
+        /// <summary>
+        /// Stops the application.
+        /// </summary>
+        public static void Stop()
+        {
+            bootstrapper.ShutDown();
+        }
+        
+        /// <summary>
+        /// Creates the kernel that will manage your application.
+        /// </summary>
+        /// <returns>The created kernel.</returns>
+        private static IKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            try
+            {
+                kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
+                kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
 
-          /// <summary>
-          /// Stops the application.
-          /// </summary>
-          public static void Stop()
-          {
-               bootstrapper.ShutDown();
-          }
+                RegisterServices(kernel);
+                return kernel;
+            }
+            catch
+            {
+                kernel.Dispose();
+                throw;
+            }
+        }
 
-          /// <summary>
-          /// Creates the kernel that will manage your application.
-          /// </summary>
-          /// <returns>The created kernel.</returns>
-          private static IKernel CreateKernel()
-          {
-               var kernel = new StandardKernel();
-               try
-               {
-                    kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
-                    kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
-
-                    RegisterServices(kernel);
-                    return kernel;
-               }
-               catch
-               {
-                    kernel.Dispose();
-                    throw;
-               }
-          }
-
-          /// <summary>
-          /// Load your modules or register your services here!
-          /// </summary>
-          /// <param name="kernel">The kernel.</param>
-          private static void RegisterServices(IKernel kernel)
-          {
-               kernel.Bind<IProductRepository>().To<EFProductRepository>();
-               //  Mock<IProductRepository> mock = new Mock<IProductRepository>();
-             //  mock.Setup(m => m.Products).Returns(new List<Product> 
-             //{ 
-             //  new Product { Name = "Football" ,Price = 23 },
-             //  new Product { Name = "Basketball" ,Price = 50 },
-             //  new Product { Name = "Golf" ,Price = 260 }             
-             //});
-             //  kernel.Bind<IProductRepository>().ToConstant(mock.Object);
-          }
-     }
+        /// <summary>
+        /// Load your modules or register your services here!
+        /// </summary>
+        /// <param name="kernel">The kernel.</param>
+        private static void RegisterServices(IKernel kernel)
+        {
+        }        
+    }
 }
